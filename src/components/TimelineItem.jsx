e955ab2c24f102f3b68b8f6ffb83b78080fe89e5
Miguel C <52728963+MiguelCamilo@users.noreply.github.com>
2022-11-21 17:32:04 -0500

    Fixed console errors

--- src/components/TimelineItem.jsx
+++ src/components/TimelineItem.jsx
@@ -7,7 +7,7 @@ function TimelineItem({ year, title, duration, details }) {
       <li className="mb-10 ml-4">
         {/* this is the timeline circles */}
         <div className="absolute w-3 h-3 bg-stone-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-stone-600 dark:bg-stone-600" />
-        <p className="flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm">
+        <div className="flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm">
           <span className="inline-block px-2 py-1 font-semibold text-white bg-stone-900 rounded-md dark:bg-white dark:text-black">
             {year}
           </span>
@@ -17,7 +17,7 @@ function TimelineItem({ year, title, duration, details }) {
           <div className="my-1 text-sm font-normal leading-none text-stone-400">
             {duration}
           </div>
-        </p>
+        </div>
         <p className="my-2 text-base font-normal text-stone-500 dark:text-stone-400">
           {details}
         </p>
