ee599370a6a1182cc05f33b41eeffa498dcf70fb
Miguel Camilo <miguelcamilo48@gmail.com>
2023-02-27 22:06:17 -0500

    CHANGED modal about section

--- src/data/modal.js
+++ src/data/modal.js
@@ -3,12 +3,13 @@ export default [
 		id: '1',
 		p1: "As a software engineer with experience in both front-end and back-end technologies, I am constantly seeking opportunities to expand my technical skills and knowledge.",
 
-		p2: "In my previous internship with Emergency Ventures, I improved the web app's responsiveness using responsive utility variants and built adaptive user interfaces for multiple platforms.",
+		p2: "In my previous journey with Emergency App, I improved the web app's responsiveness using responsive utility variants and built adaptive user interfaces for multiple platforms.",
 
-		p3: "I have also participated in a hackathon, where I worked with a team of developers to create a citizens non-emergency reporting app using React Native, React, and Node.js, and integrated API's using Fetch and ArcGIS.",
+		p3: "I have also participated in multiple hackathon. Working with a team of developers we created a citizens non-emergency reporting app which now has more than 1000 + users. Tech stack included React Native, React, and Node.js, and integrated various API's using Fetch and ArcGIS.",
 
 		p4: "In addition to my professional experience, I have completed coursework in programming logic in C at Palm Beach State College, and have completed a Full Stack Web Development Online Course through Udemy.",
         
 		p5: "I am open to remote work and am excited to take on new challenges and opportunities in the field of software engineering.",
 	},
 ];
+
